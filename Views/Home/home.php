49c3206cb93de2d95de342b0dd72fcf26237d60c
AdryHD <adryannahz04@hotmail.com>
2026-03-09 18:57:22 -0600

    Correcciones Avance 1
    Se realizan las correcciones solicitada por el profe en avance 1 para dar continuidad al avance 2

--- Views/Home/home.php
+++ Views/Home/home.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Inicio | PowerZone - Tienda Deportiva</title>
+    <link rel="icon" href="data:,">
 
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="../assets/css/lineicons.css" />
